e398d71070efeb3599fdbaec4d64cf9e06b919a1
Banstein <islambahnas1@gmail.com>
2022-02-07 14:49:09 +0200

    Create JS HTML CSS files, apply linters, Add gitignore

---
+++ main.js
@@ -0,0 +1,8 @@
+const inputTitle = document.querySelector('.input-title');
+const inputAuthor = document.querySelector('.input-author');
+const btnAdd = document.querySelector('.btn-add');
+
+btnAdd.addEventListener('click', () => {
+  const title = inputTitle.value;
+  const author = inputAuthor.value;
+});
