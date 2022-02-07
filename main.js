3eb0be5105c6c2f6e670d86edad7aa86863973c2
Banstein <islambahnas1@gmail.com>
2022-02-07 15:25:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 const inputTitle = document.querySelector('.input-title');
 const inputAuthor = document.querySelector('.input-author');
 const btnAdd = document.querySelector('.btn-add');
-let books = [];
+const books = [];
 
+const addBook = (title, author) => {
+  const id = Date.now();
+  const bookObj = { id, title, author };
+  books.push(bookObj);
+  localStorage.setItem('books', JSON.stringify(books));
+};
+
+// add Button
 btnAdd.addEventListener('click', () => {
   const title = inputTitle.value;
   const author = inputAuthor.value;
+  addBook(title, author);
 });
-
-const addBook = (title, author) => { 
-   const bookObj = {id, title, author};
-   
-   title = inputTitle.value;
-   author = inputAuthor.value;
-}
