056730929568dea34b69558b2bda42942ca8108a
Banstein <islambahnas1@gmail.com>
2022-02-07 16:04:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
+// const auth = require("registry-auth-token");
+
 const inputTitle = document.querySelector('.input-title');
 const inputAuthor = document.querySelector('.input-author');
 const btnAdd = document.querySelector('.btn-add');
 const books = [];
 
+// add Book
 const addBook = (title, author) => {
-  const id = Date.now();
-  const bookObj = { id, title, author };
-  books.push(bookObj);
-  localStorage.setItem('books', JSON.stringify(books));
+  if (title === '' || author === '') {
+    const validate = document.querySelector('.validate');
+    validate.innerHTML = 'input must be filled out';
+  } else {
+    const id = Date.now();
+    const bookObj = { id, title, author };
+    books.push(bookObj);
+    localStorage.setItem('books', JSON.stringify(books));
+    document.querySelector('.input-title').value = '';
+    document.querySelector('.input-author').value = '';
+  }
 };
 
 // add Button
@@ -15,12 +25,4 @@ btnAdd.addEventListener('click', () => {
   const title = inputTitle.value;
   const author = inputAuthor.value;
   addBook(title, author);
-
-  const validateInput = () => {
-    let inputValue = document.querySelector('ul input').value;
-    let validate = document.getElementsByClassName('.validate')
-    if(inputValue === '') {
-      validate.innerHTML = 'input must be filled out'
-    }
-  }
 });
