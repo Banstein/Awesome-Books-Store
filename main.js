a187db7803af284ae59298bacc3217d1648cd3c4
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-02-07 13:12:48 +0000

    Add addBook function

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const inputTitle = document.querySelector('.input-title');
 const inputAuthor = document.querySelector('.input-author');
 const btnAdd = document.querySelector('.btn-add');
+let books = [];
 
 btnAdd.addEventListener('click', () => {
   const title = inputTitle.value;
   const author = inputAuthor.value;
 });
+
+const addBook = (title, author) => { 
+   const bookObj = {id, title, author};
+   
+   title = inputTitle.value;
+   author = inputAuthor.value;
+}
