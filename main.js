7cdd11e41bd61747964a13675d41db7ca8314b56
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-02-09 12:41:32 +0000

    Add function to show and hide pages

--- main.js
+++ main.js
@@ -98,4 +98,38 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-// single page
+const date = document.getElementById('date');
+const { DateTime } = luxon;
+const now = DateTime.now();
+date.innerText = now.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+
+const seeListsOfBooks = document.getElementById('list-of-books');
+const addNewBook = document.getElementById('add-new-link');
+const contactLink = document.getElementById('contact-link');
+
+seeListsOfBooks.addEventListener('click', () => {
+  seeListsOfBooks.classList.add('active');
+  addNewBook.classList.remove('active');
+  contactLink.classList.remove('active');
+  document.getElementById('awesome-books').classList.remove('hide');
+  document.getElementById('add-book').classList.add('hide');
+  document.getElementById('contact-us').classList.add('hide');
+});
+
+addNewBook.addEventListener('click', () => {
+  seeListsOfBooks.classList.remove('active');
+  addNewBook.classList.add('active');
+  contactLink.classList.remove('active');
+  document.getElementById('add-book').classList.remove('hide');
+  document.getElementById('awesome-books').classList.add('hide');
+  document.getElementById('contact-us').classList.add('hide');
+});
+
+contactLink.addEventListener('click', () => {
+  seeListsOfBooks.classList.remove('active');
+  addNewBook.classList.remove('active');
+  contactLink.classList.add('active');
+  document.getElementById('contact-us').classList.remove('hide');
+  document.getElementById('awesome-books').classList.add('hide');
+  document.getElementById('add-book').classList.add('hide');
+});
