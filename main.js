e509a7d168873456820dff62539db8616bb39ac8
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-02-10 10:19:25 +0000

    Update the date function

--- main.js
+++ main.js
@@ -129,19 +129,9 @@ contactLink.addEventListener('click', () => {
   document.getElementById('add-book').classList.add('hide');
 });
 
-// date display
-function formatAMPM(date) {
-  const day = date.toDateString();
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds();
-  const ampm = hours >= 12 ? 'pm' : 'am';
-  hours %= 12;
-  hours = hours || 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? `0${minutes}` : minutes;
-  seconds = seconds < 10 ? `0${seconds}` : seconds;
-  const strTime = `${day}, ${hours}:${minutes}:${seconds} ${ampm}`;
-  return strTime;
-}
-const date = document.getElementById('date');
-date.innerText = formatAMPM(new Date());
+const showDate = () => {
+  const date = document.getElementById('date');
+  date.innerText = new Date();
+};
+
+showDate();
